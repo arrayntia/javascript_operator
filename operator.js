85c22e0313c6f058317b94b1cad8699babaf5f52
Array Ntia <arrayntia@gmail.com>
2023-02-10 21:19:31 +0100

    add file vs upload
    new commit

---
+++ operator.js
@@ -0,0 +1,37 @@
+
+//Variable declaration ---------------------------------- --------- 1
+let num_1 = 10;
+let num_2 = 5;
+
+//Using Various arithmetic operators/Print out Result  ------------ 2
+console.log (num_1 + num_2);
+
+console.log (num_1 - num_2);
+
+console.log (num_1 / num_2);
+
+console.log (num_1 * num_2);
+
+console.log (num_1 % num_2);
+
+//Use the increment/decrement operator     ----- --------------------- 3
+console.log ( ++ num_1 , --num_2)
+
+//Use prompt function    -----------------  --------------------------  4
+//The prompt only work in a browser, but running on node no result came out
+let input1 = prompt("enter num_1")
+let input2 = prompt("enter num_2")
+
+//The explicit conversion, couldn't in the node, but work well in the browser  -----  5
+let inputx = parseFloat(input1);
+
+//Arithmetic conversion            --------------------------------------------  6
+typeof("123")
+String
+typeof(Number("123"))
+number
+
+
+
+
+
